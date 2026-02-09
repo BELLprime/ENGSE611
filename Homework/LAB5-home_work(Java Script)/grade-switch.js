385b425ad7086d4bf7ec5b2d91e870c2736d8381
Bell <natthakitrodruean@gmail.com>
2026-02-09 21:19:29 +0700

    upload exercise 2 grade-switch

---
+++ Homework/LAB5-home_work(Java Script)/grade-switch.js
@@ -0,0 +1,58 @@
+function checkGrade () {
+    console.clear();
+    const output = [] ;
+    
+    let score = Math.floor(Math.random() * 101) ; //random number
+    
+    let grade = "" ;
+    if (score >= 80) {
+        grade = "A" ;
+    } else if (score >= 70) {
+        grade = "B" ;
+    } else if (score >= 60) {
+        grade = "C" ;
+    } else if (score >= 50) {
+        grade = "D" ;
+    } else {
+        grade = "F" ;
+    }
+
+    let result = score >= 50? "ผ่าน" : "ไม่ผ่าน";
+    
+    let description = "" ;
+
+    switch (grade) {
+        case "A":
+            description = "ยอดเยี่ยมมาก" ;
+            break;
+        case "B":
+            description = "ดีมาก รักษามาตราฐานไว้" ;
+            break;
+        case "C":
+            description = "ผ่านเกณฑ์ระดับดี" ;
+            break;
+        case "D":
+            description = "พอใช้ แต่ควรพัฒนาเพิ่ม" ;
+            break;
+        case "F":
+            description = "ต้องลงทะเบียนเรียนใหม่" ;
+            break;
+        default:
+            console.log("something went wrong");
+            output.push("something went wrong");
+            break;
+    }
+
+    output.push("คะแนนที่สุ่มได้: " + score);
+    console.log("คะแนนที่สุ่มได้:", score);
+    output.push("เกรด: " + grade);
+    console.log("เกรด:", grade);
+    output.push("ผลสอบ: " + result);
+    console.log("ผลสอบ:", result);
+    output.push("คำอธิบาย: " + description);
+    console.log("คำอธิบาย:", description);
+
+    document.getElementById("outputBox").textContent = output.join("\n");
+}
+
+document.getElementById("btn").addEventListener("click", checkGrade);
